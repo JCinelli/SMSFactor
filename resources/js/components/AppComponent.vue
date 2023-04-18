4b3ac44f96b772efca550753cf47b0e61c1a806e
Jeremy Cinelli <jeremy.cinelli@gmail.com>
2023-04-18 22:44:53 +0200

    initial commit

---
+++ resources/js/components/AppComponent.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1>{{ message }}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            message: "Bienvenue sur Vue 3"
+        }
+    }
+}
+</script>
+
+<style lang="css">
+h1 {
+    color: red
+}
+</style>
